3e1c8469f6cc41f64a8b76cf0ed2aab9027d558a
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2022-10-01 20:10:51 +0300

    Task 08 - done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,34 @@
+const formRef = document.querySelector('.login-form')
 
+
+formRef.addEventListener("submit", formSubmit)
+
+function formSubmit(event) {
+    event.preventDefault();
+
+    const formsElements = event.currentTarget.elements;
+    const email = formsElements.email.value;
+    const password = formsElements.password.value;
+    const data = {
+        email,
+        password,
+    }
+
+  if (email === "" || password === "") {
+        return alert("Всі поля повинні бути заповнені!");
+     }
+
+    console.log(data);
+
+    event.currentTarget.reset()
+}
+
+
+
+// const formData = new FormData(event.currentTarget)
+
+//     const data = {};
+//     formData.forEach((value, key) => {
+//         data[key] = value;
+//     });
+//     console.log(data);
