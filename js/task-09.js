50baa7ec14ca509835c786b76f75fd2ebf0a2a33
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2022-10-01 21:37:13 +0300

    Task 09 - Done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
-function getRandomHexColor() {
+ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ }
+  
+const bodyRef = document.querySelector('body') 
+const btnRef = document.querySelector('.change-color')
+const spanRef = document.querySelector('.color')
+
+btnRef.addEventListener('click', changeColor)
+
+function changeColor() {
+  const color = getRandomHexColor();
+  bodyRef.style.backgroundColor = color;
+  spanRef.textContent = color;
 }
+
+
+
+
